c3f3bdf3ebdfe87caba95f1abbd3021fb7ee1ad1
Arnaud Petit <arnaud.petit39@gmail.com>
2022-10-19 15:55:37 +0200

    full dynamics ready

--- dynamics.py
+++ dynamics.py
@@ -1,19 +1,22 @@
-from scipy.constants import hbar, k
 import numpy as np
 
+from scipy.constants import hbar, k
+
 k_B = k
-P = 1
+P = 3
 
 
-def potential(x, V_0: float, a: float):
+def potential(x: np.array, V_0: float = 1., a: float= 1.):
+    """Given potential for the problem."""
     return V_0 * ((x / a) ** 2 - 1) ** 2
 
 
-def d_potential(x, V_0: float, a: float):
+def d_potential(x: np.array, V_0: float=1., a: float=1.):
+    """Gradient of the given potential."""
     return 4 * V_0 * x * ((x / a) ** 2 - 1)
 
 
-def energy(x, v, m: float = 1.0, V_0: float = 1.0, a: float = 1.0):
+def energy(x: np.array, v: np.array, m: float = 1.0, V_0: float = 1.0, a: float = 1.0):
     """Compute potential and kinestic energy of the system."""
     kinetic_e = m * (v) ** 2 / 2
     return potential(x, V_0, a), kinetic_e
@@ -24,15 +27,17 @@ def K(M: float = 1.0, T: float = 1.0):
 
 
 def random_force(
-    v,
+    v: np.array,
     m: float = 1.0,
     gamma: float = 1.0,
     k_B: float = 1.0,
     T: float = 1.0,
     generated_number: float = np.random.random(P),
 ):
+    """Generate the Langegin random force."""
     return np.sqrt(2 * m * gamma * k_B * T) * generated_number - m * gamma * v
 
 
-def force(x, M: float, T: float):
-    return -2.0 * K(M, T)
+def force_PI(x= np.array, M: float, T: float):
+    """Path integral force."""
+    return -K(M, T) * (2*x - np.roll(x,1) - np.roll(x, -1))
