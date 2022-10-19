f14afbf990ef467db2010fc8394d623cf42913d9
Arnaud Petit <arnaud.petit39@gmail.com>
2022-10-19 16:41:04 +0200

    polish units

--- dynamics.py
+++ dynamics.py
@@ -1,33 +1,41 @@
+from scipy.constants import hbar, k
+from typing import Tuple
+
 import numpy as np
 
-from scipy.constants import hbar, k
 
+Parameters = {
+    "energy": 7.24e-21,
+    "distance": 4e-1,#Amstrom
+    "mass": 1.67e-27,
+    "time": 10e-14,
+}
 
-Parameters = {"energy": 7.24e-21, "distance": 4e-11, "mass": 1.67e-27, "time": 10e-14,}
-
-k_B = k/Parameters["energy"] #1.9e-3
-hbar = hbar/Parameters["energy"]/Parameters["time"] #1.4e-1
+k_B = k / Parameters["energy"]  # 1.9e-3
+hbar = hbar / Parameters["energy"] / Parameters["time"]  # 1.4e-1
 
 P = 3
 
 
-def potential(x: np.array, V_0: float = 1., a: float= 1.):
+def potential(x: np.array, V_0: float = 1.0, a: float = 1.0) -> np.array:
     """Given potential for the problem."""
     return V_0 * ((x / a) ** 2 - 1) ** 2
 
 
-def d_potential(x: np.array, V_0: float=1., a: float=1.):
+def d_potential(x: np.array, V_0: float = 1.0, a: float = 1.0) -> np.array:
     """Gradient of the given potential."""
     return 4 * V_0 * x * ((x / a) ** 2 - 1)
 
 
-def energy(x: np.array, v: np.array, m: float = 1.0, V_0: float = 1.0, a: float = 1.0):
+def energy(
+    x: np.array, v: np.array, m: float = 1.0, V_0: float = 1.0, a: float = 1.0
+) -> Tuple[np.array, np.array]:
     """Compute potential and kinestic energy of the system."""
     kinetic_e = m * (v) ** 2 / 2
     return potential(x, V_0, a), kinetic_e
 
 
-def K(M: float = 1.0, T: float = 1.0):
+def K(M: float = 1.0, m: float = 1.0, T: float = 1.0) -> float:
     return P * m * (k_B * T / hbar) ** 2
 
 
@@ -38,11 +46,11 @@ def random_force(
     k_B: float = 1.0,
     T: float = 1.0,
     generated_number: float = np.random.random(P),
-):
+) -> np.array:
     """Generate the Langegin random force."""
     return np.sqrt(2 * m * gamma * k_B * T) * generated_number - m * gamma * v
 
 
-def force_PI(x= np.array, M: float, T: float):
+def force_PI(x: np.array, M: float, T: float) -> np.array:
     """Path integral force."""
-    return -K(M, T) * (2*x - np.roll(x,1) - np.roll(x, -1))
+    return -K(M, T) * (2 * x - np.roll(x, 1) - np.roll(x, -1))
