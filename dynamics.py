85438918b696b785547ed37bd84f5425b88ad76a
Arnaud Petit <arnaud.petit39@gmail.com>
2022-10-26 14:07:18 +0200

    Update dynamics.py

--- dynamics.py
+++ dynamics.py
@@ -5,10 +5,10 @@ import numpy as np
 
 
 Parameters = {
-    "energy": 7.24e-21, #V_0
-    "distance": 4e-1, #Amstrom
-    "mass": 1.67e-27, #mass proton
-    "time": 10e-14, #computed to have hbar big enough
+    "energy": 7.24e-21,  # V_0
+    "distance": 4e-1,  # Amstrom
+    "mass": 1.67e-27,  # mass proton
+    "time": 10e-14,  # computed to have hbar big enough
 }
 
 k_B = k / Parameters["energy"]  # 1.9e-3
@@ -17,56 +17,40 @@ hbar = hbar / Parameters["energy"] / Parameters["time"]  # 1.4e-1
 P = 3
 
 
-def potential(x: np.array, V_0: float = 1.0, a: float = 1.0):
+def potential(x: np.array, V_0: float = 1.0, a: float = 1.0) -> np.array:
     """Given potential for the problem."""
     return V_0 * ((x / a) ** 2 - 1) ** 2
 
 
-def force_potential(x: np.array, V_0: float = 1.0, a: float = 1.0):
+def d_potential(x: np.array, V_0: float = 1.0, a: float = 1.0) -> np.array:
     """Gradient of the given potential."""
-    return - 4 * V_0 * x * ((x / a) ** 2 - 1) / (a**2 * len(x))
+    return 4 * V_0 * x * ((x / a) ** 2 - 1)
 
 
-def energy(x: np.array, v: np.array, m: float = 1.0, V_0: float = 1.0, a: float = 1.0):
+def energy(
+    x: np.array, v: np.array, m: float = 1.0, V_0: float = 1.0, a: float = 1.0
+) -> Tuple[np.array, np.array]:
     """Compute potential and kinestic energy of the system."""
     kinetic_e = m * (v) ** 2 / 2
     return potential(x, V_0, a), kinetic_e
 
 
-def K(M: float = 1.0, mass: float = 1.0, T: float = 1.0):
-    k = P * mass * (k_B * T / hbar) ** 2
-    return k
+def K(M: float = 1.0, m: float = 1.0, T: float = 1.0) -> float:
+    return P * m * (k_B * T / hbar) ** 2
 
 
 def random_force(
     v: np.array,
-    mass: float = 1.0,
+    m: float = 1.0,
     gamma: float = 1.0,
     k_B: float = 1.0,
     T: float = 1.0,
-    ):
+    generated_number: float = np.random.random(P),
+) -> np.array:
     """Generate the Langegin random force."""
-    generated_number = np.random.random(len(v))
-    random_f = np.sqrt(2 * mass * gamma * k_B * T) * generated_number - mass * gamma * v
-
-    return random_f
+    return np.sqrt(2 * m * gamma * k_B * T) * generated_number - m * gamma * v
 
 
-def force_PI(x: np.array, mass: float, T: float):
+def force_PI(x: np.array, M: float, T: float) -> np.array:
     """Path integral force."""
-    
-    return -K(mass, T) * (2 * x - np.roll(x, 1) - np.roll(x, -1))
-
-
-def force(
-    x: np.array, 
-    v:np.array, 
-    mass: float, 
-    T: float, 
-    k_B: float = 1.0, 
-    gamma: float = 1.0, 
-    V_0: float = 1.0, 
-    a: float = 1.0
-    ):
-    force_total = force_potential(x, V_0, a) + force_PI(x, mass, T) + random_force(v, mass, gamma, k_B, T)
-    return force_total
+    return -K(M, T) * (2 * x - np.roll(x, 1) - np.roll(x, -1))
