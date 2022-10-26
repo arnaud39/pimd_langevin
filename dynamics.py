ec56773d79b1061475b4715ffd814ef8bfaebcab
Arnaud Petit <arnaud.petit39@gmail.com>
2022-10-26 15:06:11 +0200

    merged works

--- dynamics.py
+++ dynamics.py
@@ -11,6 +11,7 @@ Parameters = {
     "time": 10e-14,  # computed to have hbar big enough
 }
 
+T = 0.4 * 7.24e-21 / k
 k_B = k / Parameters["energy"]  # 1.9e-3
 hbar = hbar / Parameters["energy"] / Parameters["time"]  # 1.4e-1
 
@@ -24,7 +25,7 @@ def potential(x: np.array, V_0: float = 1.0, a: float = 1.0) -> np.array:
 
 def d_potential(x: np.array, V_0: float = 1.0, a: float = 1.0) -> np.array:
     """Gradient of the given potential."""
-    return 4 * V_0 * x * ((x / a) ** 2 - 1)
+    return (4 * V_0 * x * ((x / a) ** 2 - 1)) / (-len(x) * a**2)
 
 
 def energy(
@@ -35,8 +36,8 @@ def energy(
     return potential(x, V_0, a), kinetic_e
 
 
-def K(M: float = 1.0, m: float = 1.0, T: float = 1.0) -> float:
-    return P * m * (k_B * T / hbar) ** 2
+def K(x, m: float = 1.0, T: float = 1.0) -> float:
+    return len(x) * m * (k_B * T / hbar) ** 2
 
 
 def random_force(
@@ -45,12 +46,29 @@ def random_force(
     gamma: float = 1.0,
     k_B: float = 1.0,
     T: float = 1.0,
-    generated_number: float = np.random.random(P),
 ) -> np.array:
     """Generate the Langegin random force."""
+    generated_number = np.random.random(len(v))
     return np.sqrt(2 * m * gamma * k_B * T) * generated_number - m * gamma * v
 
 
-def force_PI(x: np.array, M: float, T: float) -> np.array:
+def force_PI(x: np.array, m: float, T: float) -> np.array:
     """Path integral force."""
-    return -K(M, T) * (2 * x - np.roll(x, 1) - np.roll(x, -1))
+    return -K(x=x, m=m, T=T) * (2 * x - np.roll(x, 1) - np.roll(x, -1))
+
+
+def force(
+    x: np.array,
+    v: np.array,
+    m: float = 1.0,
+    gamma: float = 1.0,
+    V_0: float = 1.0,
+    a: float = 1.0,
+    T: float = T,
+    k_B: float = k_B,
+):
+    return (
+        random_force(v=v, m=m, gamma=gamma, k_B=k_B, T=T)
+        + force_PI(x=x, m=m, T=T)
+        + d_potential(x=x, V_0=V_0, a=a)
+    )
--- verlet.py
+++ verlet.py
@@ -2,103 +2,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import hbar, k
 from typing import Tuple
-from dynamics import *
-from dynamics_ad import *
+from dynamics import force, Parameters
 
+mass = Parameters["mass"]
 
 
 def verlet(
-    inital_positions: np.array, 
-    initial_velocities: np.array, 
-    force, 
-    iterations: int, 
-    timestep: float, 
-    mass: float, 
-    T: float,
-    k_B: float,
-    gamma: float,
-    V_0: float,
-    a: float,
-    ):
+    inital_positions: np.array,
+    initial_velocities: np.array,
+    iterations: int,
+    timestep: float,
+):
 
     positions = [inital_positions]
     velocities = [initial_velocities]
 
-    X_1 = positions[0] + timestep * velocities[0]  + timestep**2 * force(positions[0], velocities[0], mass, T, k_B, gamma, V_0, a) / (2*mass)
-    V_1 = velocities[0] + timestep * force(positions[0], velocities[0], mass, T, k_B, gamma, V_0, a)
+    X_1 = (
+        positions[0]
+        + timestep * velocities[0]
+        + timestep**2 * force(x=positions[0], v=velocities[0]) / (2 * mass)
+    )
+    V_1 = velocities[0] + timestep * force(x=positions[0], v=velocities[0])
     positions.append(X_1)
     velocities.append(V_1)
 
-    X_2 = positions[1] + timestep * velocities[1]  + timestep**2 * force(positions[1], velocities[1], mass, T, k_B, gamma, V_0, a) / (2*mass)
-    V_2 = velocities[1] + timestep * force(positions[1], velocities[1], mass, T, k_B, gamma, V_0, a)
+    X_2 = (
+        positions[1]
+        + timestep * velocities[1]
+        + timestep**2 * force(x=positions[1], v=velocities[1]) / (2 * mass)
+    )
+    V_2 = velocities[1] + timestep * force(x=positions[1], v=velocities[1])
     positions.append(X_2)
     velocities.append(V_2)
 
-    for k in range(iterations-3):
-          new_position, new_velocity = _verlet_step(positions, velocities, force, timestep, mass, T, k_B, gamma, V_0, a)
-          positions.append(new_position)
-          velocities.append(new_velocity)
+    for k in range(iterations - 3):
+        new_position, new_velocity = _verlet_step(
+            positions, velocities, timestep=timestep
+        )
+        positions.append(new_position)
+        velocities.append(new_velocity)
 
     return positions, velocities
 
 
 def _verlet_step(
-    positions, 
-    velocities, 
-    force, 
+    positions: np.array,
+    velocities: np.array,
     timestep: float,
-    mass: float, 
-    T:float,
-    k_B: float,
-    gamma: float,
-    V_0: float,
-    a: float,
-    ):
-    new_position = 2 * positions[-1] - positions[-2] + timestep**2 * force(positions[-1], velocities[-1], mass, T, k_B, gamma, V_0, a) / (2*mass)  
-    new_velocity = (3 * positions[-1] - 4 * positions[-2] + positions[-3]) / (2 * timestep)
+):
+    new_position = (
+        2 * positions[-1]
+        - positions[-2]
+        + timestep**2 * force(x=positions[-1], v=velocities[-1]) / (2 * mass)
+    )
+    new_velocity = (3 * positions[-1] - 4 * positions[-2] + positions[-3]) / (
+        2 * timestep
+    )
     return new_position, new_velocity
 
-def V_0(C, a, mass):
-    return hbar**2 / (2 * mass * a**2 * C)
 
-timestep = 10**(-17)
-mass = 1.6726 * 10**(-27)
-a = 4 * 10**(-11)
-V_0 = V_0(0.3, a, mass)
-k_B = 1.38*10**(-23)
-T = 0.4 * V_0 / k_B 
-initial_position = np.linspace(-10**(-11), -10**(-11),100)
-initial_velocities = np.linspace(0,0,100)
-force = force
-gamma = 10**12
-iterations = 1000000
-
-positions, velocities = verlet(
-    inital_positions=initial_position, 
-    initial_velocities=initial_velocities, 
-    force=force, 
-    iterations= iterations, 
-    timestep= timestep,
-    mass=mass, 
-    T=T,
-    k_B= k_B,
-    gamma= gamma,
-    V_0= V_0,
-    a=a,
-    )
-
-k = K(P=10, k_B=k_B, mass=mass, T=T)
-
-
-print(hbar)
-print(V_0)
-print(T)
-print(len(positions))
-print(k)
-
-fig, ax = plt.subplots(2, 1)
-
-time = [timestep*i for i in range(iterations)]
-ax[0].plot(time, np.array(positions).transpose()[-1])
-ax[1].plot(time, np.array(velocities).transpose()[-1])
-plt.show()
+# def V_0(C, a, mass):
+#     return hbar**2 / (2 * mass * a**2 * C)
